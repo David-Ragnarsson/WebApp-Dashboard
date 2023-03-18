a4bb9bbc1d138d6b0ae732be41a6c1dc6886b79b
David-Ragnarsson <108486409+David-Ragnarsson@users.noreply.github.com>
2023-03-18 12:58:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 const alertBanner = document.getElementById("alert");
 // create the html for the banner
-alertBanner.innerHTML = "You have unread messages"
+alertBanner.innerHTML = "Alert: You have unread messages"
